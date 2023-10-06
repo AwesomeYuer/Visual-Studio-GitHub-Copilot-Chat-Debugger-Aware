ec27e2981b70cdf33720ba5aa6bf500b1f2a0662
于斯人也 <Microshaoft@gmail.com>
2023-10-06 13:45:31 +0800

    Add project files.

---
+++ Visual-Studio-GitHub-Copilot-Chat-Debugger-Aware/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+var b = 1;
+
+var a = 1 / b;
+
+var a1 = new { a = 1, b = 2, c = 3 };
+
+var a2 = new { a = 1, b = 2, c = 3 };
+
+var a3 = a1;
+
+var a4 = DateTime.Now;
+
+int a5 = 0; // declare and initialize a5
+//a5 = 100;
+
+Console.ReadLine();
